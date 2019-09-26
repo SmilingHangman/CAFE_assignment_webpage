b1d5bfed4ba08634e6ae93eb2df1e87da5b27299
SmilingHangman <andrejus.chomicenka@gmail.com>
2019-09-26 21:51:00 +0300

    chat window imitation added

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-function openChat() {
-    document.getElementById("chatForm").style.display = "block";
+// this declaration is needed for chat window resizing later below
+var timesClicked = 0;
+
+// Chat window popup code below
+document.getElementById("chatButton").addEventListener("click", function() {
+    document.getElementById("chatButton").style.display = "none";
+    document.getElementById("chatForm").style.bottom = "0";
+    document.getElementById("topP").style.visibility = "hidden";
+    document.getElementById("chatMid").style.display = "block";
+    document.getElementById("chatTop").style.height = "270px";
+    timesClicked = 0;
+}
+);
+
+// Chat window close code below
+document.getElementById("closeChat").addEventListener("click", function() {
+    setTimeout(function(){document.getElementById("chatButton").style.display = "block"}, 300);
+    document.getElementById("chatForm").style.bottom = "-540px";
+}
+);
+
+// Chat window resizing code below
+
+document.getElementById("chatTop").addEventListener("click", function() {
+timesClicked++;
+
+if (timesClicked%2==0) {
+        document.getElementById("topP").style.visibility = "hidden";
+        setTimeout(function(){document.getElementById("chatMid").style.display = "block"}, 200);
+        document.getElementById("chatTop").style.height = "270px";
+} else {
+        setTimeout(function(){document.getElementById("topP").style.visibility = "visible"}, 200);
+        document.getElementById("chatMid").style.display = "none";
+        document.getElementById("chatTop").style.height = "60px";
 }
 
-function closeChat() {
-    document.getElementById("chatForm").style.display = "none";
-}
+});
